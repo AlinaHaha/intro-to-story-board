40457cd443caa3bc8e144115ec789f9c72778531
Alina Hassan <42146262+AlinaHaha@users.noreply.github.com>
2020-08-05 11:33:31 -0400

    Add files via upload

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Alina Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        //this is a print statemnt
+}
+}
